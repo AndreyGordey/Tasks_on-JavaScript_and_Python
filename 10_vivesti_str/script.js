97a443cb1c4a20a3919901656afc4b0e31f33f0b
AndreyGordey <multiistomin@gmail.com>
2025-04-26 16:33:02 +0300

    10

---
+++ 10_vivesti_str/script.js
@@ -0,0 +1,12 @@
+// Ввод строки от пользователя
+const string = prompt("Введите строку:");
+
+// Вывод строки три раза через запятую
+const result = `${string}, ${string}, ${string}`;
+
+// Подсчёт количества символов в строке
+const length = string.length;
+
+// Вывод результата
+console.log(`Результат: ${result}`);
+console.log(`Количество символов в строке: ${length}`);
